76a8c315e74dd1a3e4c759816d1714e874e817d1
Hevin Jantasmin <hjantasmin@bangkit.com>
2024-10-02 12:02:15 -0700

    updated app ver

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
-APP_VERSION = '1.0.0'
+APP_VERSION = '1.0.1'
 
-# ASSETS_BASE_DIR = 'S:/ECOM-CC-WHS/master_files'
-ASSETS_BASE_DIR = './'
-UOM_MASTER_FILENAME = 'test_uom_input.csv'
+ASSETS_BASE_DIR = 'S:/ECOM-CC-WHS/master_files'
+# ASSETS_BASE_DIR = './'
+UOM_MASTER_FILENAME = 'uom_input.csv'
+QTY_PRICE_MASTER_FILENAME = 'Available Qty Whse 01 + Price Levels.xlsx'
+LOGS_FILENAME = 'logs_avc.txt'
 USER_DOWNLOADS = str(Path.home() / "Downloads") + '/'
 OUTPUT_DIR = './avc_outputs/'
