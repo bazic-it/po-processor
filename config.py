28e239f1830c74d697e02df7e198854a31aca479
Hevin Jantasmin <hjantasmin@bangkit.com>
2025-02-04 09:18:47 -0800

    implemented external config.json

--- config.py
+++ config.py
@@ -1,11 +1,39 @@
 from pathlib import Path
+import json
 
-APP_VERSION = '1.0.9'
+def readConfigJson():
+    configs = {
+        "assets_base_directory": "",
+        "uom_master_filename": "",
+        "inventory_master_filename": "",
+        "logs_filename": "",
+        "local_logs_filepath": "",
+        "input_file_location": "",
+        "output_file_location": ""
+    }
 
-ASSETS_BASE_DIR = 'S:/ECOM-CC-WHS/master_files'
+    jsonFilepath = "./assets/config.json"
+
+    with open(jsonFilepath, 'r') as file:
+        data = json.load(file)
+        configs["assets_base_directory"] = data["assets_base_directory"]
+        configs["uom_master_filename"] = data["uom_master_filename"]
+        configs["inventory_master_filename"] = data["inventory_master_filename"]
+        configs["logs_filename"] = data["logs_filename"]
+        configs["local_logs_filepath"] = data["local_logs_filepath"]
+        configs["input_file_location"] = data["input_file_location"]
+        configs["output_file_location"] = data["output_file_location"]
+
+    return configs
+
+configs = readConfigJson()
+
+APP_VERSION = '1.1.0'
+
+ASSETS_BASE_DIR = configs["assets_base_directory"]
 # ASSETS_BASE_DIR = './'
-UOM_MASTER_FILENAME = 'uom_input.xlsx'
-QTY_PRICE_MASTER_FILENAME = 'Available Qty Whse 01 + Price Levels.xlsx'
-LOGS_FILENAME = 'logs_avc.txt'
-USER_DOWNLOADS = str(Path.home() / "Downloads") + '/'
-OUTPUT_DIR = './avc_outputs/'
+UOM_MASTER_FILENAME = configs["uom_master_filename"]
+QTY_PRICE_MASTER_FILENAME = configs["inventory_master_filename"]
+LOGS_FILENAME = configs["logs_filename"]
+USER_DOWNLOADS = str(Path.home() / configs["input_file_location"]) + '/'
+OUTPUT_DIR = configs["output_file_location"]
